2058b4edfe7461ba83dc11e4d5337042e67484b5
Olefir Timofey <tima.tima.tima.2000@mail.ru>
2025-09-10 17:57:35 +0700

    KS-1-2 create and print new variables

---
+++ src/main/kotlin/lesson_1/task_1-2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val textOfGratitude: String = "Thanks for purchase!"
+
+    println(numberOfOrders)
+    println(textOfGratitude)
+
+    var numberOfEmployees: Int = 2000
+
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+}
