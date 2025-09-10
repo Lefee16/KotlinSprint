394705d0d4fd6dfdc1337760472e6a99c8299570
Olefir Timofey <tima.tima.tima.2000@mail.ru>
2025-09-10 19:18:33 +0700

    KS-2-1 average value

---
+++ src/main/kotlin/lesson_2/task_2-1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val studentGrade1 = 3.0
+    val studentGrade2 = 4.0
+    val studentGrade3 = 3.0
+    val studentGrade4 = 5.0
+
+    val result = (studentGrade1 + studentGrade2 + studentGrade3 + studentGrade4) / 4
+    println(result)
+
+}
