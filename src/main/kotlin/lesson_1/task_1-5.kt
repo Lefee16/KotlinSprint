055fc1ed08a8f05815b068881312bb69e1eb9a44
Olefir Timofey <tima.tima.tima.2000@mail.ru>
2025-09-11 17:36:03 +0700

    KS-1-5 average value corrected

--- src/main/kotlin/lesson_1/task_1-5.kt
+++ src/main/kotlin/lesson_1/task_1-5.kt
@@ -3,11 +3,14 @@ package org.example.lesson_1
 fun main() {
 
     val timeIsSpace = 6480
-    val hoursInSpace = timeIsSpace / 3600
-    val minutesInSpace = (timeIsSpace % 3600) / 60
-    val secondsInSpace = timeIsSpace % 60
+    val hoursInSpace = timeIsSpace / TIME_IN_3600
+    val minutesInSpace = (timeIsSpace % TIME_IN_3600) / TIME_IN_60
+    val secondsInSpace = timeIsSpace % TIME_IN_60
     val result = "%02d:%02d:%02d".format(hoursInSpace, minutesInSpace, secondsInSpace)
 
     println(result)
 
 }
+
+const val TIME_IN_60 = 60
+const val  TIME_IN_3600 = 3600
