f2dcf0fc01e8820b20c2656d6c27a7fed3ea1fb6
Olefir Timofey <tima.tima.tima.2000@mail.ru>
2025-09-11 19:39:27 +0700

    KS-2-1 corrected

--- src/main/kotlin/lesson_2/task_2-1.kt
+++ src/main/kotlin/lesson_2/task_2-1.kt
@@ -6,8 +6,9 @@ fun main() {
     val studentGrade2 = 4.0
     val studentGrade3 = 3.0
     val studentGrade4 = 5.0
+    val numberOfStudents = 4
 
-    val result = (studentGrade1 + studentGrade2 + studentGrade3 + studentGrade4) / 4
+    val result = (studentGrade1 + studentGrade2 + studentGrade3 + studentGrade4) / numberOfStudents
     println(result)
 
 }
